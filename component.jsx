548876629eaafc1d85d12677b2cecae8e8eb4887
AndreyVorobiev88 <Foroven@mail.ru>
2025-03-06 12:57:08 +0300

    modified:   CSS.css 	new file:   CharacterScheme.json 	modified:   component.jsx

--- component.jsx
+++ component.jsx
@@ -1,10 +1,18 @@
 
-import 'CSS.css';
+import "CSS.css"
   function CharacterComponent(props) {
 
     return (
-    <div>
-        <img src ="/Resources/Necromancer.JPG" className={"image-radius"}></img>;
-    </div>
+        <div id="main" className="main_div_style">
+
+            //Картинка с персонажем
+            <div>
+                <img src ="/Resources/Necromancer.JPG" className={"image-radius"}></img>;
+            </div>
+
+            //Основные данные персонажа
+            <div>"Имя: " + props.name</div>
+    
+        </div>
     )
   }
